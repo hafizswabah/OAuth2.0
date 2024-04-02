0bc1abdc8a1365cb0fd6d9ca6b3bcca5718d614c
hafizswabah <swabah.a.avd121@gmail.com>
2024-04-02 12:49:56 +0530

    Login page created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,26 +1,12 @@
+import Login from "./Pages/Login/Login";
+import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
-import axios from 'axios'
-import { Link } from 'react-router-dom';
 function App() {
-
-  function navigate(url) {
-    window.location.href = url
-  }
-
-  async function handLogin() {
-    let data = await axios.post('http://localhost:8888/user/request')
-    navigate(data.data.url)
-  }
-
   return (
-    <>
-      <h1>Login using google accounts</h1>
-
-      <button onClick={handLogin}>Login</button>
-
-    </>
-  )
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ Client/src/Pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+    return (
+        <>
+            <div className="Login-page">
+                <div className="Login-Box">
+                    <h2>Login</h2>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
