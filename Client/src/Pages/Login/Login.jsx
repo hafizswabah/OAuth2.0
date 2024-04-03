afaf00dd26405ff653b312bee6bff01ae558e3a8
hafizswabah <swabah.a.avd121@gmail.com>
2024-04-03 14:56:18 +0530

    Login page created

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Login.css'
-
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 function Login() {
+    function navigate(url) {
+        window.location.href = url;
+      }
+
+    async function handLogin() {
+        let data = await axios.post("http://localhost:8888/user/request")
+     navigate(data.data.url)
+    }
+
     return (
         <>
             <div className="Login-page">
                 <div className="Login-Box">
                     <h2>Login</h2>
+                    <button onClick={handLogin}>Login</button>
                 </div>
             </div>
         </>
