a598ea9d606d6be495d1e248b5873e2fdfb79b9c
hafizswabah <swabah.a.avd121@gmail.com>
2024-04-04 17:08:29 +0530

    google user details fetched succesfully

--- Server/controller/user.js
+++ Server/controller/user.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 export async function auth(req, res) {
 console.log('auth route works');
-    const redirectUrl = "http://localhost:3000"
+    const redirectUrl = "http://localhost:8888/user/auth"
 
     const oAuth2Client = new OAuth2Client(
         process.env.CLIENT_ID,
@@ -21,24 +21,27 @@ console.log('auth route works');
 }
 
 async function getUserData(access_tocken) {
-    let data = await axios.get(`https://www.googoleapi.com/oauth2/v3/userinfo?access_token${access_tocken}`)
-    console.log('data', data);
+    let data = await axios.get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_tocken}`);
+
+    console.log('user-data :::', data);
 }
 export async function authentication(req, res) {
     let code = req.query.code;
     try {
-        const redirectUrl = "http://localhost:3000"
+        const redirectUrl = "http://localhost:8888/user/auth"
         const oAuth2Client = new OAuth2Client(
             process.env.CLIENT_ID,
             process.env.CLIENT_SECRETE,
             redirectUrl
         )
-        const res = await oAuth2Client.getToken(code);
-        await oAuth2Client.setCredentials(res.tokens);
+        const response = await oAuth2Client.getToken(code);
+        await oAuth2Client.setCredentials(response.tokens);
         console.log('token acquired');
         const user = oAuth2Client.credentials;
         console.log('creditials', user);
         await getUserData(user.access_token)
+        res.json({err:false,message:"user details fetched succesfully"})
+      
     } catch (err) {
         console.log('login err', err);
     }
