2a857825a468c2d5e7f83490d619875dcb7ef650
hafizswabah <swabah.a.avd121@gmail.com>
2024-04-04 12:50:16 +0530

    login page ui updated

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -5,11 +5,11 @@ import { useNavigate } from 'react-router-dom'
 function Login() {
     function navigate(url) {
         window.location.href = url;
-      }
+    }
 
     async function handLogin() {
         let data = await axios.post("http://localhost:8888/user/request")
-     navigate(data.data.url)
+        navigate(data.data.url)
     }
 
     return (
@@ -17,7 +17,18 @@ function Login() {
             <div className="Login-page">
                 <div className="Login-Box">
                     <h2>Login</h2>
-                    <button onClick={handLogin}>Login</button>
+                    <div className="login-details-area">
+                        <div className="login-details">
+                            <label htmlFor="">User Name</label>
+                            <input type="text" className='login-input' />
+                        </div>
+                        <div className="login-details">
+                            <label htmlFor="">Password</label>
+                            <input type="text" className='login-input' />
+                        </div>
+                        <button className='login-btn' onClick={handLogin}>Login</button>
+                    </div>
+
                 </div>
             </div>
         </>
